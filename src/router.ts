c009d82045bfd3a306400d8b7bd6ac56582bc9d1
telsangvi <vishwa.telsang@gmail.com>
2024-03-03 00:11:33 +0530

    feat(routes): setup initial app config and routes

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,14 @@
-import express from 'express'
-import usersRoutes from './users/routes/userRoutes'
-import transactionsRoutes from './transactions/routes/transactionsRoutes'
-import categoriesRoutes from './categories/routes/categoriesRoutes'
-import summaryRoutes from './summary/routes/summaryRoutes'
+import express from 'express';
+import usersRoutes from './users/routes/userRoutes';
+import transactionsRoutes from './transactions/routes/transactionsRoutes';
+import categoriesRoutes from './categories/routes/categoriesRoutes';
+import summaryRoutes from './summary/routes/summaryRoutes';
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.use('/users',usersRoutes)
-routes.use('/transactions',transactionsRoutes)
-routes.use('/categories',categoriesRoutes)
-routes.use('/summary',summaryRoutes)
+routes.use('/users', usersRoutes);
+routes.use('/transactions', transactionsRoutes);
+routes.use('/categories', categoriesRoutes);
+routes.use('/summary', summaryRoutes);
 
-export default routes
+export default routes;
